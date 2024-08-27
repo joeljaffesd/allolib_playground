1ccb2b6ae83a47978bd1032264c80f13d10cbdfb
Joel Jaffe <joel@jaffesd.com>
2024-08-27 09:27:25 -0700

    Customized init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,10 @@ rm -rf allolib
 rm -rf al_ext
 
 git submodule update --init --recursive
+
+sed -i '' '7a\ 
+#include <cstring>' ./allolib/src/ui/al_Parameter.cpp
+
+sudo apt install build-essential git git-lfs cmake libsndfile1-dev libassimp-dev libasound-dev libxrandr-dev libopengl-dev libxinerama-dev libxcursor-dev libxi-dev
+sudo apt-get install libgl1-mesa-dev xorg openbox
+export MESA_GL_VERSION_OVERRIDE=4.5
