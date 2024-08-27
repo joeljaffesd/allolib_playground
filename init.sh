52ebd6285b5723db516ef05b9aa4af1415fcd282
Joel Jaffe <joel@jaffesd.com>
2024-08-27 09:27:25 -0700

    Customized init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ rm -rf allolib
 rm -rf al_ext
 
 git submodule update --init --recursive
+
+sudo apt install build-essential git git-lfs cmake libsndfile1-dev libassimp-dev libasound-dev libxrandr-dev libopengl-dev libxinerama-dev libxcursor-dev libxi-dev
+sudo apt-get install libgl1-mesa-dev xorg openbox
+export MESA_GL_VERSION_OVERRIDE=4.5
